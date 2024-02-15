dd77acacfad40dec7fe2c039354cce83bac53897
OlehSirbu <olegsirbu2@gmail.com>
2024-02-15 18:48:49 +0100

    remove unused code

--- src/state/reducers/quizQuestionsReducer.ts
+++ src/state/reducers/quizQuestionsReducer.ts
@@ -1,5 +1,4 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { IQuestion, IQuizQuestionsState } from "src/types";
 
 const initialState: IQuizQuestionsState = {
   loading: true,
